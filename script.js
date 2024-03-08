35ac8f1e3a6dd0b498acc6d6542592ebfcad0481
Machuki Clinton <48854561+Machukichuxx@users.noreply.github.com>
2024-03-08 16:10:15 +0000

    moving the circle

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-function myKeyDown(event){
-    console.log(event.keyCode);
-
-}
-document.addEventListener('keydown',myKeyDown);
+//excersise 9 question 1
+document.addEventListener('keydown', function(event) {
+    var elm = document.getElementById('circle');
+    var computedStyle = window.getComputedStyle(elm);
+    var circleMarginTop = parseFloat(computedStyle.marginTop);
+    var circleMarginLeft = parseFloat(computedStyle.marginLeft);
+    if (event.keyCode === 37) { // Left arrow key
+      elm.style.marginLeft = (circleMarginLeft - 10) + 'px';
+    }
+    if (event.keyCode === 38) { // Up arrow key
+      elm.style.marginTop = (circleMarginTop - 10) + 'px';
+    } 
+    if (event.keyCode === 39) { // Right arrow key
+      elm.style.marginLeft = (circleMarginLeft + 10) + 'px';
+    }
+    if (event.keyCode === 40) { // Down arrow key
+      elm.style.marginTop = (circleMarginTop + 10) + 'px';
+    }
+  });
+  
