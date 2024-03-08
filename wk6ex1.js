93ace2a6a0977dd34f108323416183a721c0ef73
Machuki Clinton <48854561+Machukichuxx@users.noreply.github.com>
2024-03-08 16:46:26 +0000

    ARRAYS

---
+++ wk6ex1.js
@@ -0,0 +1,25 @@
+ var module ={
+    "course" : "MSc Computing",
+    "module" : "CSYM019",
+    "moduleName" : "Internet Programming",
+    "tutor" : {
+        "firstName" : "Clinton",
+        "secondName" : "Machuki",
+        "email" : "clintonmachuki@gmail.com"
+
+    }
+ };
+
+ alert(module.course);
+
+
+ ///Array
+
+ var module = [
+    'CSYM017',
+    'CSYM018',
+    'CSYM019'
+
+ ]
+
+ alert(module[1])
